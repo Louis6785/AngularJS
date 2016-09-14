e14f2829462b9bba8130644aa9cd176934e10a86
Louis6785 <louis_shi@jabil.com>
2016-09-14 16:07:28 +0800

    改以ng-submit方式執行

--- app/startup.js
+++ app/startup.js
@@ -1,9 +1,11 @@
 var App = angular.module("myApp",[]);
 
 App.controller("StartUpController",function($scope) {
-    $scope.funding = { startingEstimate: 0 };
-    computeNeeded = function() {
-        $scope.funding.needed = $scope.funding.startingEstimate * 10;
+    $scope.computeNeeded = function() {
+        $scope.needed = $scope.startingEstimate * 10;
     };
-    $scope.$watch("funding.startingEstimate",computeNeeded);
+
+    $scope.requestFunding = function() {
+        window.alert("Sorry, please get more customers first.");
+    };
 });
