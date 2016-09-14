017b3dacf288c612c31e74edbb2040c426b7f3a9
Louis6785 <louis_shi@jabil.com>
2016-09-14 15:50:53 +0800

    添加ng-bind方式

--- app/hello.js
+++ app/hello.js
@@ -1,5 +1,8 @@
 var App = angular.module("myApp",[]);
 
 App.controller("HelloController",function($scope) {
+    var someText = {};
+    someText.message = "You have started your joumey.";
+    $scope.someText = someText;
     $scope.greeting = { text: "Hello" };
 });
