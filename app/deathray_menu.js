3981b4e6e9e4fb3797d526bd1472d699d0e69fce
Louis6785 <louis_shi@jabil.com>
2016-09-19 16:24:46 +0800

    添加class的控制

--- app/deathray_menu.js
+++ app/deathray_menu.js
@@ -1,6 +1,11 @@
 var App = angular.module("myApp",[]);
 
 App.controller("DeathrayMenuController",function($scope) {
+    $scope.isDisabled = false;
+    $scope.stun = function() {
+        $scope.isDisabled = "true";
+    };
+
     $scope.menuState = { "show" : false };
     $scope.toggleMenu = function() {
         $scope.menuState.show = !$scope.menuState.show;
