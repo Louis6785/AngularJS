ff4728274230808a7e781190e2692350291f4e96
Louis6785 <louis_shi@jabil.com>
2016-09-19 15:48:40 +0800

    添加ng-repeat範例

---
+++ app/student_list.js
@@ -0,0 +1,14 @@
+var App = angular.module("myApp",[]);
+
+var students = [
+    { name : "Mary Contrary", id : "1" },
+    { name : "Jack Sprat", id : "2" },
+    { name : "Jill Hill", id : "3" }
+];
+
+App.controller("StudentListController",function($scope) {
+    $scope.students = students;
+    $scope.insertTom = function() {
+        $scope.students.splice(1,0,{ name : "Tom Thumb", id : "4" });
+    };
+});
